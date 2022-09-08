5071dfdc81f5b0634ff0be166d60f9c63e1625e0
mrsitech <mrsitech@outlook.com>
2022-09-08 20:32:15 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
-import './styles.css'
 function NotificationButton() {
     return (
         <div className="dsmeta-red-btn">
@@ -9,4 +9,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
