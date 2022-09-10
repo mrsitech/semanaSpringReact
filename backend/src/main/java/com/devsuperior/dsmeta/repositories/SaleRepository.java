ff5fc724a5c6524bb09118236e7a615b921cde08
mrsitech <mrsitech@outlook.com>
2022-09-09 22:40:37 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
@@ -8,5 +13,9 @@ import com.devsuperior.dsmeta.entities.Sale;
 //Salva e Captura elementos no DataBase
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
+	//Pesquisa no DB e recebe as datas como argumento
+	//JPQL lenguage
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 	
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -18,8 +23,16 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		//A Data max do calendário, if vier vazio, é a de hj, senão é a data que o cliente
+		//passou.
+		LocalDate max = maxDate.equals("") ? today: LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 		
 	}
 
