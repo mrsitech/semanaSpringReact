0f8990312a6e4e2ee11085b165bef1efc01304af
mrsitech <mrsitech@outlook.com>
2022-09-15 23:11:06 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -23,7 +23,7 @@ function SalesCard() {
     const dmin = minDate.toISOString().slice(0, 10);
 
     const dmax = maxDate.toISOString().slice(0, 10);
-    console.log(dmin); 
+   
 
 
     axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
@@ -79,29 +79,13 @@ function SalesCard() {
                 <td>R$ {sale.amount.toFixed(2)}</td>
                 <td>
                   <div className="dsmeta-red-btn-container">
-                    <NotificationButton />
+                    <NotificationButton  saleId={sale.id} />
                   </div>
                 </td>
               </tr>)
             }) 
               
             }
-            
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <div className="dsmeta-red-btn-container">
-                    <NotificationButton />
-                  </div>
-                </div>
-              </td>
-            </tr>
           </tbody>
 
         </table>
